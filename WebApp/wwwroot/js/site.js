16b11195b2894f17aa5019fb4e7fb16034493538
Evgeniy.Kosjakov <elv1s42k@gmail.com>
2023-01-31 18:40:55 +0200

    random icons

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -9,3 +9,28 @@ function scrollToId(id) {
     });
   }
 }
+
+function addRandomIcons() {
+  const divs = document.getElementsByClassName('random-icon');
+  const parent = document.getElementById('random-icons-container');
+  const winWidth = parent.offsetWidth;
+  const winHeight = parent.offsetHeight;
+  
+  for (var i = 0; i < divs.length; i++) {
+    
+    var thisDiv = divs[i];
+    
+    var randomTop = getRandomNumber(0, winHeight);
+    var randomLeft = getRandomNumber(0, winWidth);
+
+    thisDiv.style.top = randomTop + "px";
+    thisDiv.style.left = randomLeft + "px";
+  }
+}
+
+// function that returns a random number between a min and max
+function getRandomNumber(min, max) {
+
+  return Math.random() * (max - min) + min;
+
+}
